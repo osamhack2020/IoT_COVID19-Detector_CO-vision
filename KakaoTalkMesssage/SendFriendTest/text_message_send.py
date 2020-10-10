7a8081f976560cce05d44e385c250dd5b870eda6
DongJun-H <dongjun826@seoultech.ac.kr>
2020-10-10 17:04:21 +0900

    카카오톡 메시지 템플릿 테스트 및 친구간 메시지 보내기 테스트

--- KakaoTalkMesssage/SendFriendTest/text_message_send.py
+++ KakaoTalkMesssage/SendFriendTest/text_message_send.py
@@ -1,42 +1,43 @@
 import json
 import requests
 
-url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+get_friend_list_url = 'https://kapi.kakao.com/v1/api/talk/friends'
+send_friend_url = 'https://kapi.kakao.com/v1/api/talk/friends/message/default/send'
 # 사용자 토큰
 token = 'REST API' #REST API
 accessToken = 'accessToken'
 
 def getFriendList(accessToken) :
-    url = 'https://kapi.kakao.com/v1/api/talk/friends'
+    get_friend_list_url = 'https://kapi.kakao.com/v1/api/talk/friends'
     payload = ''
     headers = {
         'Content-Type' : "application/x-www-form-urlencoded",
         'Cache-Control' : "no-cache",
         'Authorization' : "Bearer " + str(accessToken),
     }
-    response = requests.request("GET",url,data=payload, headers=headers)
+    response = requests.request("GET",get_friend_list_url,data=payload, headers=headers)
     print(response)
     friend_List = json.loads(((response.text).encode('utf-8')))
     friend_UUID_List = []
     elements = response.json().get('elements')
     for element in elements:
-        print(element.get("uuid"))
+        #print(element.get("uuid"))
         friend_UUID_List.append(element.get("uuid"))
-    #print(friend_UUID_List)
-    return friend_List
+    print(friend_UUID_List)
+    return friend_UUID_List[0]
 
-#print(getFriendList(accessToken))
+print(getFriendList(accessToken))
 
 headers = {
     'Authorization' : "Bearer " + str(accessToken)
 }
 
 data = {
-  'receiver_uuids': '[friend_UUID_List[0]]', #친구 UID 입력
+  'receiver_uuids': '[""]', #친구 UID 입력
   'template_object': '{ "object_type": "text", "text": "텍스트 영역", "link": { "web_url": "https://developers.kakao.com", "mobile_web_url": "https://developers.kakao.com" }, "button_title": "타이틀 영역" }'
 }
 
-response = requests.post(url, data=data, headers=headers)
+response = requests.post(send_friend_url, data=data, headers=headers)
 print(response.status_code)
 if response.json().get('result_code') == 0:
     print('메시지를 성공적으로 보냈습니다.')
