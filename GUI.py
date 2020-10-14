55a193fde09b5938dd91510e030e1d301b40b187
Donghyeon Cho <hyeon9698@naver.com>
2020-10-14 23:43:17 +0900

    tesing GUI

--- GUI.py
+++ GUI.py
@@ -1,124 +1,73 @@
-import cv2
+#https://github.com/berrouba-med-amine/simple-python-camera-viewer-opencv3-PyQt5/blob/master/main_window.py
+# import system module
 import sys
-from PyQt5 import QtCore
-from PyQt5 import QtWidgets
-from PyQt5 import QtGui
 
+# import some PyQt5 modules
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QImage
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QTimer
 
-class ShowVideo(QtCore.QObject):
+# import Opencv module
+import cv2
 
-    flag = 0
+from ui_main_window import *
 
-    camera = cv2.VideoCapture('imgs/junha_video.mp4')
-    
+class MainWindow(QWidget):
+    # class constructor
+    def __init__(self):
+        # call QWidget constructor
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
 
-    ret, image = camera.read()
-    image = cv2.resize(image, dsize=(0, 0), fx=0.3, fy=0.3, interpolation=cv2.INTER_AREA)
-    height, width = image.shape[:2]
+        # create a timer
+        self.timer = QTimer()
+        # set timer timeout callback function
+        self.timer.timeout.connect(self.viewCam)
+        # set control_bt callback clicked  function
+        self.ui.control_bt.clicked.connect(self.controlTimer)
 
-    VideoSignal1 = QtCore.pyqtSignal(QtGui.QImage)
-    VideoSignal2 = QtCore.pyqtSignal(QtGui.QImage)
+    # view camera
+    def viewCam(self):
+        # read image in BGR format
+        ret, image = self.cap.read()
+        # convert image to RGB format
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # get image infos
+        height, width, channel = image.shape
+        step = channel * width
+        # create QImage from image
+        qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
+        # show image in img_label
+        self.ui.image_label.setPixmap(QPixmap.fromImage(qImg))
 
-    def __init__(self, parent=None):
-        super(ShowVideo, self).__init__(parent)
-
-    @QtCore.pyqtSlot()
-    def startVideo(self):
-        global image
-
-        run_video = True
-        while run_video:
-            ret, image = self.camera.read()
-            image = cv2.resize(image, dsize=(0, 0), fx=0.3, fy=0.3, interpolation=cv2.INTER_AREA)
-            color_swapped_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-            qt_image1 = QtGui.QImage(color_swapped_image.data,
-                                    self.width,
-                                    self.height,
-                                    color_swapped_image.strides[0],
-                                    QtGui.QImage.Format_RGB888)
-            self.VideoSignal1.emit(qt_image1)
-
-
-            if self.flag:
-                img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                img_canny = cv2.Canny(img_gray, 50, 100)
-
-                qt_image2 = QtGui.QImage(img_canny.data,
-                                         self.width,
-                                         self.height,
-                                         img_canny.strides[0],
-                                         QtGui.QImage.Format_Grayscale8)
-
-                self.VideoSignal2.emit(qt_image2)
-
-
-            loop = QtCore.QEventLoop()
-            QtCore.QTimer.singleShot(25, loop.quit) #25 ms
-            loop.exec_()
-
-    @QtCore.pyqtSlot()
-    def canny(self):
-        self.flag = 1 - self.flag
-
-
-class ImageViewer(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super(ImageViewer, self).__init__(parent)
-        self.image = QtGui.QImage()
-        self.setAttribute(QtCore.Qt.WA_OpaquePaintEvent)
-
-    def paintEvent(self, event):
-        painter = QtGui.QPainter(self)
-        painter.drawImage(0, 0, self.image)
-        self.image = QtGui.QImage()
-
-    def initUI(self):
-        self.setWindowTitle('Test')
-
-    @QtCore.pyqtSlot(QtGui.QImage)
-    def setImage(self, image):
-        if image.isNull():
-            print("Viewer Dropped frame!")
-
-        self.image = image
-        if image.size() != self.size():
-            self.setFixedSize(image.size())
-        self.update()
+    # start/stop timer
+    def controlTimer(self):
+        # if timer is stopped
+        if not self.timer.isActive():
+            # create video capture
+            self.cap = cv2.VideoCapture('imgs/junha_video.mp4')
+            # start timer
+            self.timer.start(20)
+            # update control_bt text
+            self.ui.control_bt.setText("Stop")
+        # if timer is started
+        else:
+            # stop timer
+            self.timer.stop()
+            # release video capture
+            self.cap.release()
+            # update control_bt text
+            self.ui.control_bt.setText("Start")
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
 
+    # create and show mainWindow
+    mainWindow = MainWindow()
+    mainWindow.show()
 
-    thread = QtCore.QThread()
-    thread.start()
-    vid = ShowVideo()
-    vid.moveToThread(thread)
-
-    image_viewer1 = ImageViewer()
-    image_viewer2 = ImageViewer()
-
-    vid.VideoSignal1.connect(image_viewer1.setImage)
-    vid.VideoSignal2.connect(image_viewer2.setImage)
-
-    push_button1 = QtWidgets.QPushButton('Start')
-    push_button2 = QtWidgets.QPushButton('Canny')
-    push_button1.clicked.connect(vid.startVideo)
-    push_button2.clicked.connect(vid.canny)
-
-    vertical_layout = QtWidgets.QVBoxLayout()
-    horizontal_layout = QtWidgets.QHBoxLayout()
-    horizontal_layout.addWidget(image_viewer1)
-    horizontal_layout.addWidget(image_viewer2)
-    vertical_layout.addLayout(horizontal_layout)
-    vertical_layout.addWidget(push_button1)
-    vertical_layout.addWidget(push_button2)
-
-    layout_widget = QtWidgets.QWidget()
-    layout_widget.setLayout(vertical_layout)
-
-    main_window = QtWidgets.QMainWindow()
-    main_window.setCentralWidget(layout_widget)
-    main_window.show()
     sys.exit(app.exec_())
